307400d0a9ac5eb6d99cc48212388e2ebf7fb0dc
Amey Dhoke <amey16.dhoke@gmail.com>
2024-04-13 21:16:37 +0530

    Axios request is now working

--- src/components/MainWindow.tsx
+++ src/components/MainWindow.tsx
@@ -1,19 +1,23 @@
 import React, {useState} from "react";
 import api from "../api";
+import { AxiosRequestConfig } from "axios";
 
 const MainWindow = () => {
     const [text, setText] = useState('');
     const [summarizedText, setSummary] = useState('');
 
-    const summarizeText = async(text: any) => {
-        const response = await api.get('/text/summarize', text);
-        setSummary(response.data)
+    const summarizeText = async(text: string) => {
+        console.log(text)
+        const response = await api.get('/text/summarize', {params:{text_to_summarize: text}});
+        console.log(response)
+        setSummary(response.data.result)
     }
 
     
 
     const handleOnChange = (event: any) => {
         setText(event.target.value);
+        console.log(text)
     }
 
     return (
@@ -23,7 +27,7 @@ const MainWindow = () => {
                     className="max-h-80 w-full p-4 rounded resize-none my-5 border-solid border-blue-600"
                     placeholder="Paste your text here..." onChange={handleOnChange}
                 ></textarea>
-                <button className={`bg-slate-200 w-auto ${text == ''? 'cursor-not-allowed' : '' }`} onClick={summarizeText} >Summarize</button>
+                <button className={`bg-slate-200 w-auto ${text == ''? 'cursor-not-allowed' : '' }`} onClick={() => summarizeText(text)} >Summarize</button>
             </div>
             <div className="bg-pink-500 border-2 border-solid w-2/5 border-black h-52 flex gap-2 items-center flex-shrink-0">
                 <h1>This is the output of summarization</h1>

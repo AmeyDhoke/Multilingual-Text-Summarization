f5d690d8d3ecfa3f7e42659e76589435b5f65ce7
Dhoke Mahendra (PAC3MXD) <PAC3MXD@aad.ups.com>
2024-04-18 22:09:44 +0530

    Front-end modifications

--- src/components/MainWindow.tsx
+++ src/components/MainWindow.tsx
@@ -1,33 +1,58 @@
-import React, {useState} from "react";
+import {useState} from "react";
 import api from "../api";
 
 const MainWindow = () => {
     const [text, setText] = useState('');
     const [summarizedText, setSummary] = useState('');
+    const [language, setLanguage] = useState(undefined);
 
-    const summarizeText = async(text: any) => {
-        const response = await api.get('/text/summarize', text);
+    // const summarizeText = async(text: any) => {
+    //     const response = await api.get('/text/summarize', text);
+    //     setSummary(response.data)
+    // }
+
+    const summarizeMultiText = async(text: any, language?: any) => {
+        const response = await api.get('/text/summarize2', { params: {text: text , lang: language}});
         setSummary(response.data)
     }
 
+    const handleSelect = (event: any) => {
+        setLanguage(event.target.value);
+    }
     
 
     const handleOnChange = (event: any) => {
         setText(event.target.value);
     }
 
+    const lang_options = [
+        {label: "English", value: "english"},
+        {label: "Hindi", value: "hindi"},
+        {label: "Marathi", value: "marathi"},
+    ]
+
+
     return (
         <div className="h-screen flex gap-4 justify-evenly items-center bg-cyan-100">
             <div className="w-2/5 px-6 py-5  border-2 border-solid border-black h-96 rounded-lg bg-teal-300">
                 <textarea
-                    className="max-h-80 w-full p-4 rounded resize-none my-5 border-solid border-blue-600"
+                    className="h-1/2 w-full p-4 rounded resize-none my-5 border-solid border-blue-600"
                     placeholder="Paste your text here..." onChange={handleOnChange}
                 ></textarea>
-                <button className={`bg-slate-200 w-auto ${text == ''? 'cursor-not-allowed' : '' }`} onClick={summarizeText} >Summarize</button>
+                <div>
+                <p>Select output language</p>
+                <select onChange={handleSelect}>
+                    {lang_options.map(option =>(
+                    <option value = {option.value}>{option.label}</option>
+                    ))}
+                </select>
+                </div>
+                <button className={`bg-slate-200 w-auto ${text == ''? 'cursor-not-allowed' : '' } mt-5`} onClick={() => summarizeMultiText(text, language)} >Summarize</button>
             </div>
-            <div className="bg-pink-500 border-2 border-solid w-2/5 border-black h-52 flex gap-2 items-center flex-shrink-0">
+            <div className="bg-pink-500 px-6 py-5 border-2 border-solid w-2/5 border-black h-96 rounded-lg flex flex-col gap-2 items-center flex-shrink-0">
                 <h1>This is the output of summarization</h1>
-                <textarea className="max-h-80 w-full p-4 rounded resize-none my-5 border-solid border-blue-600" value={summarizedText}></textarea>
+                <textarea className="h-full w-full p-4 rounded resize-none my-5 border-solid border-blue-600 bg-white" disabled value={summarizedText}></textarea>
+
             </div>
         </div>
     );
